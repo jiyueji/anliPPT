332fc0ab7db8d2d789b81cb5b5ffab6a12ac5132
zhangwei <zhangwei>
2020-01-16 13:49:57 +0800

    bug

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 var changeIndex = 1;
 
-var srcArr1 = ["./html/monthlyReport/salesHome.html","./html/monthlyReport/map.html", "./html/monthlyReport/histogram.html"]
+var srcArr1 = ["./html/monthlyReport/salesHome.html", "./html/monthlyReport/map.html", "./html/monthlyReport/histogram.html"]
 var srcArr2 = ["./html/agpKpi/formTab/formTab.html", "./html/agpKpi/aboSegmentMonthlyData.html", "./html/agpKpi/ppv.html", "./html/agpKpi/endPpv.html"]
 var srcArr3 = ["./html/aboMomentum/force/force.html", "./html/aboMomentum/pf20QMonth.html", "./html/aboMomentum/income.html"]
 var srcArr4 = ["./html/predictionModel/abo.html", "./html/predictionModel/bonusMigrationModel.html", "./html/predictionModel/pinMigrationModel.html"]
 var srcArr5 = ["./html/dailyReport/dailySales.html", "./html/dailyReport/buyer/buyer.html", "./html/dailyReport/csi.html"]
-var srcArr6 = ["./html/hourlyReport/newSales.html","./html/hourlyReport/foaSales.html","./html/hourlyReport/amountComm.html", "./html/hourlyReport/referralAmount.html"]
+var srcArr6 = ["./html/hourlyReport/newSales.html", "./html/hourlyReport/foaSales.html", "./html/hourlyReport/amountComm.html", "./html/hourlyReport/referralAmount.html"]
 
+var srcArrAll = ["./html/monthlyReport/salesHome.html", "./html/monthlyReport/map.html", "./html/monthlyReport/histogram.html", "./html/agpKpi/formTab/formTab.html", "./html/agpKpi/aboSegmentMonthlyData.html", "./html/agpKpi/ppv.html", "./html/agpKpi/endPpv.html", "./html/aboMomentum/force/force.html", "./html/aboMomentum/pf20QMonth.html", "./html/aboMomentum/income.html", "./html/predictionModel/abo.html", "./html/predictionModel/bonusMigrationModel.html", "./html/predictionModel/pinMigrationModel.html", "./html/dailyReport/dailySales.html", "./html/dailyReport/buyer/buyer.html", "./html/dailyReport/csi.html", "./html/hourlyReport/newSales.html", "./html/hourlyReport/foaSales.html", "./html/hourlyReport/amountComm.html", "./html/hourlyReport/referralAmount.html"]
 
 var containerUpBig = new Swiper('.swiper-containerUpBig', {
     loop: true,
@@ -33,9 +34,9 @@ var containerUpBig = new Swiper('.swiper-containerUpBig', {
         //     // alert("111")
         //     //你的事件
         // },
-        slideChangeTransitionStart: function() {
+        slideChangeTransitionStart: function () {
             var nowSlides = document.getElementsByClassName("swiper-slide-active")[0]
-                // alert(this.realIndex)
+            // alert(this.realIndex)
             if (changeIndex == 1) {
                 nowSlides.firstElementChild.src = srcArr1[this.realIndex]
             } else if (changeIndex == 2) {
@@ -49,6 +50,9 @@ var containerUpBig = new Swiper('.swiper-containerUpBig', {
             } else if (changeIndex == 6) {
                 nowSlides.firstElementChild.src = srcArr6[this.realIndex]
             }
+            //  else if (changeIndex == 7) {
+            //     nowSlides.firstElementChild.src = srcArrAll[this.realIndex]
+            // }
         },
     },
     // navigation: {
@@ -74,11 +78,11 @@ var containerDownSmall = new Swiper('.swiper-containerDownSmall', {
     slideToClickedSlide: true,
     // noSwiping: true, //设置为true时禁止切换
     spaceBetween: 15,
-          //在slide之间设置距离 默认单位px 也可以设置% vw vh ....
+    //在slide之间设置距离 默认单位px 也可以设置% vw vh ....
     centeredSlides: true,
-         //设定为true时，active slide会居中，而不是默认状态下的居左
+    //设定为true时，active slide会居中，而不是默认状态下的居左
     slidesOffsetBefore: 170,
-        //设定slide与左边框的预设偏移量 还有slidesOffsetAfter
+    //设定slide与左边框的预设偏移量 还有slidesOffsetAfter
     observer: true, //修改swiper自己或子元素时，自动初始化swiper
     coverflowEffect: {
         rotate: -10, // rotate：slide做3d旋转时Y轴的旋转角度。默认50。
@@ -158,23 +162,73 @@ var containerDownSmall = new Swiper('.swiper-containerDownSmall', {
 // var aaa = document.getElementById("aaa")
 // aaa.addEventListener('click', test)
 
-var carouselInterval;
-function startCarousel() {
-  if(carouselInterval !== undefined) return false;
-  carouselInterval = setInterval(function() {
-    console.log('carousel interval');
-  }, 1000);
-}
-function stopCarousel() {
-  clearInterval(carouselInterval);
-  carouselInterval = undefined;
-}
+// var carouselInterval;
+// function startCarousel() {
+//     console.log(containerUpBig)
+//     changeIndex = 7;
+//     // containerUpBig.destroy(true,true); //移除所有slide监听事件，所以拖动和click事件失效了。
+//     var changeBinner = document.getElementById("changeBinner")
+//     changeBinner.innerHTML = ""
+//     for (var i = 0; i < srcArrAll.length; i++) {
+//         var iframe = document.createElement("iframe");
+//         iframe.className = "iframeStyle";
+//         iframe.frameborder = "no";
+//         iframe.marginwidth = "0"
+//         iframe.marginheight = "0"
+//         iframe.scrolling = "no"
+//         iframe.align = "center"
+//         iframe.src = srcArrAll[i];
+//         // if()
+//         var mask = document.createElement("div");
+//         mask.className = 'mask';
+//         var div = document.createElement("div");
+//         div.className = "swiper-slide"
+//         div.appendChild(iframe);
+//         div.appendChild(mask);
+//         changeBinner.appendChild(div)
+//     }
+//     var containerUpBig = new Swiper('.swiper-containerUpBig', {
+//         loop: true,
+//         effect: 'coverflow',
+//         slidesPerView: 3,
+//         freeMod: false,
+//         initialSlide: 0, //初始展示
+//         slideToClickedSlide: true,
+//         centeredSlides: true, //设置slide居中
+//         slidesOffsetBefore: -245,
+//         observer: true, //修改swiper自己或子元素时，自动初始化swiper
+//         // 　　　　 observeParents: true,//修改swiper的父元素时，自动初始化swiper
+//         //         observeSlideChildren:true,
+//         coverflowEffect: {
+//             rotate: 50, // rotate：slide做3d旋转时Y轴的旋转角度。默认50。
+//             stretch: -50, //stretch：每个slide之间的拉伸值，越大slide靠得越紧。 默认0。
+//             depth: 120, //depth：slide的位置深度。值越大z轴距离越远，看起来越小。 默认100。
+//             modifier: 1, //modifier：depth和rotate和stretch的倍率，相当于depth*modifier、rotate*modifier、stretch*modifier，值越大这三个参数的效果越明显。默认1。
+//             slideShadows: false //slideShadows：开启slide阴影。默认 true。
+//         },
+//         autoplay: {//自动切换
+//             delay: 3000,
+//             stopOnLastSlide: false,
+//             disableOnInteraction: true,
+//         },
+//     })
+
+
+    //   if(carouselInterval !== undefined) return false;
+    //   carouselInterval = setInterval(function() {
+    //     console.log('carousel interval');
+    //   }, 1000);
+// }
+// function stopCarousel() {
+//     clearInterval(carouselInterval);
+//     carouselInterval = undefined;
+// }
 
 // 标题替换
 var tatleChange = document.getElementById("tatleChange")
 
 function oneImg() {
-    
+
     console.log("111")
     changeIndex = 1;
     tatleChange.innerHTML = "Sales Performance"
@@ -241,24 +295,24 @@ function twoImg() {
         iframe.align = "center"
         iframe.src = srcArr2[i];
         // if (i == 0) {
-            // var mask1 = document.createElement("div");
-            // var mask2 = document.createElement("div");
-            // mask1.className = 'mask-01';
-            // mask2.className = 'mask-02';
-            // var div = document.createElement("div");
-            // div.className = "swiper-slide"
-            // div.appendChild(iframe);
-            // div.appendChild(mask1);
-            // div.appendChild(mask2);
-            // changeBinner.appendChild(div)
+        // var mask1 = document.createElement("div");
+        // var mask2 = document.createElement("div");
+        // mask1.className = 'mask-01';
+        // mask2.className = 'mask-02';
+        // var div = document.createElement("div");
+        // div.className = "swiper-slide"
+        // div.appendChild(iframe);
+        // div.appendChild(mask1);
+        // div.appendChild(mask2);
+        // changeBinner.appendChild(div)
         // } else {
-            var mask = document.createElement("div");
-            mask.className = 'mask';
-            var div = document.createElement("div");
-            div.className = "swiper-slide"
-            div.appendChild(iframe);
-            div.appendChild(mask);
-            changeBinner.appendChild(div)
+        var mask = document.createElement("div");
+        mask.className = 'mask';
+        var div = document.createElement("div");
+        div.className = "swiper-slide"
+        div.appendChild(iframe);
+        div.appendChild(mask);
+        changeBinner.appendChild(div)
         // }
     }
     var containerUpBig = new Swiper('.swiper-containerUpBig', {
